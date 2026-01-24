18c770553d2ed99fc5a72f7a6c62eede7ba70236
AliA12336 <amadams12336@gmail.com>
2026-01-24 16:02:01 -0500

    Update variable names

--- src/scripts/convertImages.ts
+++ src/scripts/convertImages.ts
@@ -2,12 +2,12 @@ type ImageModule = {
 	default: string;
 };
 
-const images = import.meta.glob<ImageModule>(
+const potpourriImages = import.meta.glob<ImageModule>(
 	"/src/assets/images/potpourri/**/*.{png,jpg,jpeg,webp,svg}",
 	{ eager: true },
 );
 
-export const galleryImages = Object.entries(images).map(([path, module]) => {
+export const galleryImages = Object.entries(potpourriImages).map(([path, module]) => {
 	const parts = path.split("/");
 	const folder = parts[parts.length - 2];
 	const filename = parts[parts.length - 1];
