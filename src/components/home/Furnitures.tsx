78c8650fbad0ff672ee5f2b2256ad73f74cd9cae
AliA12336 <amadams12336@gmail.com>
2026-02-14 12:53:24 -0500

    Separate furnitures component

--- src/components/home/Furnitures.tsx
+++ src/components/home/Furnitures.tsx
@@ -1,1 +1,26 @@
-export const Furnitures = () => {};
+import type { RefObject } from "react";
+import furnitures from "../../images.json";
+import { Furniture } from "./Furniture";
+import { getRandomIntInclusive } from "./TestScene";
+
+const RANDOMMIN = -50;
+const RANDOMMAX = 50;
+
+export const Furnitures = ({
+	canvasRef,
+}: {
+	canvasRef: RefObject<HTMLDivElement | null>;
+}) => {
+	const projects = furnitures.map(({ glb: [glb] }) => glb);
+
+    return (
+	projects.map((project) => {
+		return (
+			<Furniture
+				key={project}
+				initialPosition={[getRandomIntInclusive(RANDOMMIN, RANDOMMAX), 250, 0]}
+				canvasRef={canvasRef}
+				glbPath={project}
+			/>
+		)
+    }))}
--- src/components/home/TestScene.tsx
+++ src/components/home/TestScene.tsx
@@ -1,8 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
 import { Suspense, useRef } from "react";
-import furnitures from "../../images.json";
-import { Furniture } from "./Furniture";
+import { Furnitures } from "./Furnitures";
 
 export const getRandomIntInclusive = (min: number, max: number) => {
 	const minCeiled = Math.ceil(min);
@@ -14,7 +13,6 @@ const RANDOMMIN = -50;
 const RANDOMMAX = 50;
 
 export const TestScene = () => {
-	const projects = furnitures.map(({ glb: [glb] }) => glb);
 	const canvasRef = useRef<HTMLDivElement>(null);
 	const gravity = getRandomIntInclusive(RANDOMMIN, RANDOMMAX);
 
@@ -24,21 +22,8 @@ export const TestScene = () => {
 				<ambientLight intensity={0.1} />
 				<directionalLight color="#dfdedf" position={[0, 0, 5]} />
 				<Suspense fallback={null}>
-					<Physics gravity={[gravity, -20, 0]} debug>
-						{projects.map((project) => {
-							return (
-								<Furniture
-									key={project}
-									initialPosition={[
-										getRandomIntInclusive(RANDOMMIN, RANDOMMAX),
-										250,
-										0,
-									]}
-									canvasRef={canvasRef}
-									glbPath={project}
-								/>
-							);
-						})}
+					<Physics gravity={[gravity, -20, 0]}>
+						<Furnitures canvasRef={canvasRef} />
 					</Physics>
 				</Suspense>
 			</Canvas>
