9e4bd9ffad9b577a77171201e94238ed2a98f640
AliA12336 <amadams12336@gmail.com>
2026-02-16 20:30:04 -0500

    Add max num of furniture rendered

--- src/components/home/Furnitures.tsx
+++ src/components/home/Furnitures.tsx
@@ -2,14 +2,28 @@ import type { RefObject } from "react";
 import furnitures from "../../images.json";
 import { Furniture } from "./Furniture";
 
+const MAXNUMPROJECTS = 8;
+// Fisher-yates shuffle algo
+function shuffle(arr: string[]) {
+	const copy = [...arr];
+
+	for (let i = copy.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[copy[i], copy[j]] = [copy[j], copy[i]];
+	}
+
+	return copy;
+}
+
 export const Furnitures = ({
 	canvasRef,
 }: {
 	canvasRef: RefObject<HTMLDivElement | null>;
 }) => {
 	const projects = furnitures.map(({ glb: [glb] }) => glb);
+	const randomProjects = shuffle(projects).slice(0, MAXNUMPROJECTS);
 
-	return projects.map((project, index) => {
+	return randomProjects.map((project, index) => {
 		return (
 			<Furniture
 				key={project}
